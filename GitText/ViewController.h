9aca6eba960d59a6f601836d9633ac0f0f54307f
Aa1234567 <540602120@qq.com>
2016-01-19 16:32:26 +0800

    Initial Commit

---
+++ GitText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitText
+//
+//  Created by qfsy on 16/1/19.
+//  Copyright © 2016年 qfsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitText/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitText
+//
+//  Created by qfsy on 16/1/19.
+//  Copyright © 2016年 qfsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
